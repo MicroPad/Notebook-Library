46529de4538a093a616437ea2df4b79c8680dce4
Nick Webster <nick@nick.geek.nz>
2018-07-03 14:10:44 +1200

    Removing since ignored files

--- dist/Asset.d.ts
+++
@@ -1,8 +0,0 @@
-export default class Asset {
-    data: Blob;
-    readonly uuid: string;
-    constructor(data: Blob, uuid?: string);
-    toString(): Promise<string>;
-    toXmlObject(): Promise<any>;
-    private generateGuid;
-}
